4858a6d37712fc17ab02b7d82211d1679b9deea6
Hans Jørgen Hoel <hansjorg@urkilden.no>
2017-06-30 11:11:24 +0200

    Run apt-get update before apt-get install

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
 
 function ai {
   apt-get install -qq -y $@ > /dev/null
 }
 
 cat ~ubuntu/.git_logs_HEAD | grep "clone:" | head -n 1 | sed -rn 's/^\w+ \w+ ([^<]+)(<.*| [0-9]{5}).*/\1/p' > /etc/who_am_i
+
+apt-get update
 
 echo "Installing nginx"
 ai nginx
