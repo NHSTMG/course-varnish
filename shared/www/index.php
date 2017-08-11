117fc6e87806c14aa6e2567545bc6e5b8cd3bd3d
reidarok <reidar@cloudware.no>
2017-08-11 14:39:27 +0200

    Deleting all cookies on http://localhost:10080/

--- shared/www/index.php
+++ shared/www/index.php
@@ -1,3 +1,16 @@
+<?php
+  /* Delete all cookies. */
+  if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+  }
+?>
+
 Hi! Put any HTML or web resources here, and it will be served by nginx at http://localhost:10080/ on your host machine.
 
-<?php print "PHP is enabled."; ?>
+<?php print "PHP is enabled. By the way, all cookies on this domain have also been deleted."; ?>
