d0b63bf8b013f05fb0106c87b617229d05108116
reidarok <reidar@cloudware.no>
2017-08-11 14:40:46 +0200

    Spelling.

--- shared/www/index.php
+++ shared/www/index.php
@@ -11,6 +11,6 @@
   }
 ?>
 
-Hi! Put any HTML or web resources here, and it will be served by nginx at http://localhost:10080/ on your host machine.
+Hi! Put any HTML or web resources here, and they will be served by nginx at http://localhost:10080/ on your host machine.
 
 <?php print "PHP is enabled. By the way, all cookies on this domain have also been deleted."; ?>
