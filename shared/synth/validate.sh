939de60d0fa9eccfb0f04cb426ebac6e1bf19709
Hans Jørgen Hoel <hansjorg@urkilden.no>
2017-09-11 15:28:43 +0200

    Fix validate.sh for synth

--- shared/synth/validate.sh
+++ shared/synth/validate.sh
@@ -6,7 +6,7 @@ require_status_code "http://localhost:6081/synth/error/" 418
 
 require_body_content "http://localhost:6081/synth/content/" "This is the file content.html"
 
-require_status_code "http://localhost:6081/synth/error/" 404
+require_status_code "http://localhost:6081/synth/content/" 404
 
 require_status_code "http://localhost:6081/synth/redirect/" 302
 
