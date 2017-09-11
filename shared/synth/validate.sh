3f979bc84a73ac89abdd9993a4b96a5617428a52
Hans Jørgen Hoel <hansjorg@urkilden.no>
2017-09-11 14:30:51 +0200

    Complete shared/synth/validate.sh

--- shared/synth/validate.sh
+++ shared/synth/validate.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
-
+set -e
 source ~/shared/.scripts/base
 
+require_status_code "http://localhost:6081/synth/error/" 418
+
+require_body_content "http://localhost:6081/synth/content/" "This is the file content.html"
+
+require_status_code "http://localhost:6081/synth/error/" 404
+
+require_status_code "http://localhost:6081/synth/redirect/" 302
+
+require_header_value "http://localhost:6081/synth/redirect/" "Location" "synth/content"
+
 report_success
+
