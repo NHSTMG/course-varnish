e84504e72c2ccc5de6b4b324d1c110f37d8341f7
Reidar Øksnevad <reidar.oksnevad@nhst.no>
2017-09-12 12:57:32 +0200

    Add syntax highlighting for VCL files, using the nano editor.

--- setup/always_run_on_vagrant_up.sh
+++ setup/always_run_on_vagrant_up.sh
@@ -3,3 +3,5 @@
 export DEBIAN_FRONTEND=noninteractive
 
 /etc/init.d/nginx restart
+cp -n ~ubuntu/shared/.config/vcl.nanorc /usr/share/nano/vcl.nanorc
+cp -n ~ubuntu/shared/.config/.nanorc ~ubuntu/.nanorc
