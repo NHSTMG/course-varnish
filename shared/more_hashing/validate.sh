da2e75c5b1bd0712dc34a88b826731aebc970107
Hans Jørgen Hoel <hansjorg@urkilden.no>
2017-09-12 19:49:46 +0200

    Add more_hashing

--- shared/more_hashing/validate.sh
+++ shared/more_hashing/validate.sh
@@ -4,13 +4,13 @@ source ~/shared/.scripts/base
 
 ping_server "http://localhost:6081" "Varnish server on localhost"
 
-if ! curl -H "Referer: /hash_referer/color.php" -s "http://localhost:6081/hash_referer/tile.php?i=1" | grep -q "referrer=color2"
+if ! curl -H "Referer: /more_hashing/color.php" -s "http://localhost:6081/more_hashing/tile.php?i=1" | grep -q "referrer=color2"
 then
   echo "Didn't get a color tile for color.php referrer."
   fail_exercise
 fi
 
-if ! curl -H "Referer: /hash_referer/blackwhite.php" -s "http://localhost:6081/hash_referer/tile.php?i=1" | grep -q "referrer=blackwhite2"
+if ! curl -H "Referer: /more_hashing/blackwhite.php" -s "http://localhost:6081/more_hashing/tile.php?i=1" | grep -q "referrer=blackwhite2"
 then
   echo "Didn't get a black&white tile for blackwhite.php referrer."
   fail_exercise
